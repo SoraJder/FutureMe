8b7d33a0e1d39fe06b4c848b1bff2b75546cf290
Alina <alinateca2001@gmail.com>
2023-01-27 19:33:14 +0200

    Added colors and fonts

--- app/src/main/java/com/alina/futureme/presentation/theme/Color.kt
+++ app/src/main/java/com/alina/futureme/presentation/theme/Color.kt
@@ -2,7 +2,13 @@ package com.alina.futureme.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val PrimaryColor: Color = Color(0XFF64B5F6)
+val PrimaryLightColor: Color = Color(0XFF9BE7FF)
+val PrimaryDarkColor: Color = Color(0XFF2286C3)
+
+val SecondaryColor: Color = Color(0XFFD1C4E9)
+val SecondaryLightColor: Color = Color(0XFFFFF7FF)
+val SecondaryDarkColor: Color = Color(0XFFA094B7)
+
+val PrimaryTextColor: Color = Color(0XFF000000)
+val SecondaryTextColor: Color = Color(0XFF000000)
--- app/src/main/java/com/alina/futureme/presentation/theme/Type.kt
+++ app/src/main/java/com/alina/futureme/presentation/theme/Type.kt
@@ -2,27 +2,63 @@ package com.alina.futureme.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.alina.futureme.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    h1 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_light)),
+        fontSize = 96.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_semi_bold)),
+        fontSize = 60.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_regular)),
+        fontSize = 48.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_regular)),
+        fontSize = 34.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_bold)),
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_medium)),
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_regular)),
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_medium)),
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_regular)),
+        fontSize = 18.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_regular)),
+        fontSize = 14.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+        fontFamily = FontFamily(Font(R.font.work_sans_medium)),
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = FontFamily(Font(R.font.work_sans_regular)),
         fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = FontFamily(Font(R.font.work_sans_semi_bold)),
+        fontSize = 12.sp,
     )
-    */
 )
