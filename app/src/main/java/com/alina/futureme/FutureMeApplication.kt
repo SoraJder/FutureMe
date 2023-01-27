f35e584dcd34e24ed480c26e34f4b9df1d5deb84
Alina <alinateca2001@gmail.com>
2023-01-27 19:03:41 +0200

    Updated dependencies

--- app/src/main/java/com/alina/futureme/FutureMeApplication.kt
+++ app/src/main/java/com/alina/futureme/FutureMeApplication.kt
@@ -1,6 +1,7 @@
 package com.alina.futureme
 
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FutureMeApplication
+class FutureMeApplication:Application()
