f66e087c09fc1301f0433aee3d341cca178eb0ae
Alina <alinateca2001@gmail.com>
2023-01-28 19:15:18 +0200

    Added sha1 key

--- app/src/main/java/com/alina/futureme/domain/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/alina/futureme/domain/repository/AuthenticationRepository.kt
@@ -1,5 +1,12 @@
 package com.alina.futureme.domain.repository
 
+import com.google.firebase.auth.AuthResult
+import io.grpc.internal.SharedResourceHolder.Resource
+import kotlinx.coroutines.flow.Flow
+
 interface AuthenticationRepository {
+   suspend fun getCurrentUser(): Resource<Boolean>
+    suspend fun signInWithEmail(email:String, password:String):Resource<AuthResult>
+   suspend  fun signUpWithEmail(email:String, password: String):Resource<AuthResult>
 
 }
