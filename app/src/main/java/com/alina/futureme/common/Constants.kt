17d7d6761576083a016f74395e8ac280ab6ce33b
Alina <alinateca2001@gmail.com>
2023-02-17 15:30:10 +0200

    Implemented the navigation manager

--- app/src/main/java/com/alina/futureme/common/Constants.kt
+++ app/src/main/java/com/alina/futureme/common/Constants.kt
@@ -2,7 +2,7 @@ package com.alina.futureme.common
 
 object Constants {
 
-    //Screems
+    //Screens
     const val SIGN_IN_SCREEN ="Sign in"
     const val SIGN_UP_SCREEN = "Sign up"
     const val HOME_SCREEN="Home"
--- app/src/main/java/com/alina/futureme/navigation/ScreensDestination.kt
+++ app/src/main/java/com/alina/futureme/navigation/ScreensDestination.kt
@@ -4,8 +4,6 @@ import com.alina.futureme.common.Constants.HOME_SCREEN
 import com.alina.futureme.common.Constants.SIGN_IN_SCREEN
 import com.alina.futureme.common.Constants.SIGN_UP_SCREEN
 
-sealed class Screen (val route: String){
-    object SignInScreen : Screen(SIGN_IN_SCREEN)
-    object SignUPScreen: Screen(SIGN_UP_SCREEN)
-    object HOME :Screen(HOME_SCREEN)
-}
+object SignInScreen: Destination(SIGN_IN_SCREEN)
+object SignUpScreen: Destination(SIGN_UP_SCREEN)
+object HomeScreen: Destination(HOME_SCREEN)
